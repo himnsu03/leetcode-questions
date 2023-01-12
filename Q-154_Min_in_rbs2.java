6ec73f5cfe3ed5e584392c97ab1222eb8e7fa578
Himanshu Sharma <113498220+himnsu03@users.noreply.github.com>
2023-01-12 12:25:09 +0530

    Create Q-154_Min_in_rbs2.java

---
+++ Q-154_Min_in_rbs2.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int pivot = findPivotWithDuplicates(nums);
+        if(pivot == (nums.length-1)){
+            return nums[0];
+        }
+        else{
+            return nums[pivot+1];
+        }
+    }
+    static int findPivotWithDuplicates(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+
+            // if elements at middle, start, end are equal then just skip the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                // skip the duplicates
+             
+                if (start < end && arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                // check whether end is pivot
+                if (end > start && arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
