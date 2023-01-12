1a7af83c665b01db7786eaa465a1008fb4397e94
Himanshu Sharma <113498220+himnsu03@users.noreply.github.com>
2023-01-12 12:21:44 +0530

    Create Q-153_Min_in_rbs.java

---
+++ Q-153_Min_in_rbs.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int pivot = Pivot(nums);
+        if(pivot == (nums.length-1)){
+            return 0;
+        }
+        else {
+            return nums[pivot + 1] ;
+        }
+    }
+    public int Pivot(int[] nums){
+        int start = 0 ;
+        int end = nums.length - 1;
+        while(start<end){
+            int mid = start + (end - start) / 2;
+            
+            if ( mid<end && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && nums[mid] < nums[mid-1]) {
+                return mid-1;
+            }
+            if (nums[mid] <= nums[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+        }
+    }
