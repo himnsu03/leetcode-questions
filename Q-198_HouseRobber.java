bde30e82a55584dfbca542ef6199227e1d899cc4
Himanshu Sharma <113498220+himnsu03@users.noreply.github.com>
2023-06-14 23:09:56 +0530

    Create Q-198_HouseRobber.java

---
+++ Q-198_HouseRobber.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int n = nums.length;
+        
+        // Base cases for the first two houses
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        // Calculate the maximum amount for each house
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        
+        return dp[n];
+    }
+}
