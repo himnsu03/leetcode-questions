60a2f1b3d42f4e96edeec8ac609281a2c9324521
Himanshu Sharma <113498220+himnsu03@users.noreply.github.com>
2023-11-14 15:51:24 +0530

    Create Q342_powOfFour.java

---
+++ Q342_powOfFour.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public boolean isPowerOfFour(int n) {
+        
+        if (n <= 0) {
+            return false;
+        }
+
+        return n == 1 || (n % 4 == 0 && isPowerOfFour(n / 4));
+    }
+}
