63c06f9feac360b21a8c77044086fb92209f2193
Himanshu Sharma <113498220+himnsu03@users.noreply.github.com>
2023-11-23 00:43:54 +0530

    Create leetcode39.java

---
+++ leetcode39.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates); 
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+
+     static void backtrack(List<List<Integer>> result, List<Integer> current, int[] candidates, int remaining, int start) {
+        if (remaining == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] > remaining) {
+                
+                break;
+            }
+
+            current.add(candidates[i]);
+            
+            backtrack(result, current, candidates, remaining - candidates[i], i);
+            current.remove(current.size() - 1);
+        }
+    }
+}
