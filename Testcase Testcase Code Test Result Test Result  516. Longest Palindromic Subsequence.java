28acedbff6de105b99b05248a79dc425c4e0fafb
Himanshu Sharma <113498220+himnsu03@users.noreply.github.com>
2023-12-08 09:30:10 +0530

    Create  Testcase Testcase Code Test Result Test Result  516. Longest Palindromic Subsequence.java

---
+++ Testcase Testcase Code Test Result Test Result  516. Longest Palindromic Subsequence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        StringBuilder rev = new StringBuilder(s);
+        rev.reverse();
+        String reverse = rev.toString();
+        return longestCommonSubsequence(s,reverse);
+    }
+    public int longestCommonSubsequence(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
